50a8502bfbf1076e0b6ff67c17599eeb4d0fae43
BryceKoehl <63129946+BryceKoehl@users.noreply.github.com>
2021-09-28 22:39:45 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import 'semantic-ui-css/semantic.css';
 import { Container, Header, Button, Grid, Segment, Form, Select, ButtonOr, List, Label, GridColumn, } from "semantic-ui-react";
+// import NewTaskForm from "../components/NewTaskForm";
 
 import Task from '../components/Tasks';
 
@@ -23,43 +24,9 @@ const HomePage = () => {
             <Button textAlign='right' color='green' icon='plus'></Button>
           </Grid.Column>
         </Grid>
-
-        <Segment>
-          <Header as='h2'>
-            New Task
-          </Header>
-
-          <Form>
-            <Form.Field
-              control='input'
-              label='Task Name'
-              placeholder='Enter task name...'
-            />
-
-            <Form.Field
-              control={Select}
-              label='Task Color'
-              placeholder='Choose task color...'
-              options={[
-                { text: 'Red', Value: 'red' },
-                { text: 'Yellow', Value: 'yellow' },
-                { text: 'Green', Value: 'green' },
-              ]}
-            />
-
-            <Button.Group fluid>
-
-              <Button color='red'>Cancel</Button>
-
-              <ButtonOr />
-
-              <Button color='green'>Add Task</Button>
-
-            </Button.Group>
-
-          </Form>
-        </Segment>
-
+        {/* 
+        {true ? <NewTaskForm /> : null
+        } */}
         <List>
 
           <Task name='Task 1' color='yellow'></Task>
@@ -67,9 +34,8 @@ const HomePage = () => {
           <Task name='Task 3' color='green'></Task>
 
         </List>
-
       </Container>
-    </React.Fragment>
+    </React.Fragment >
   );
 };
 
